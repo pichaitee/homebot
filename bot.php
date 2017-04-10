3a7dafd37c6e20d10cc1798c4dd64be82fa0d172
pichaitee <te_tcs@hotmail.com>
2017-04-10 18:11:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,57 @@
 <?php
+<script src="https://cdn.netpie.io/microgear.js"></script>
+
+<script>
+
+  const APPID = "YouNETPIEAppID";
+  const KEY = "b5uB1JoFA1rjBRb";
+  const SECRET = "Vc2fBchO5S39uWL9WxpfRQbLi";
+
+  const ALIAS = "DigitalOUTPUT_HTML_web";     //  ชื่อตัวเอง
+  const thing1 = "NodeMCU1";                                   //  ชื่อเพื่อนที่จะคุย
+
+  function switchPress(logic){
+    if(logic == 1 ){
+      microgear.chat(thing1,"ON");
+    }else if(logic == 0 ){
+      microgear.chat(thing1,"OFF");
+    }
+  }
+
+  var microgear = Microgear.create({
+    key: KEY,
+    secret: SECRET,
+    alias : ALIAS
+  });
+
+
+  microgear.on('message', function(topic,data) {
+    if(data=="ON"){
+      document.getElementById("Status").innerHTML =  "Load is ON.";
+    }else if(data=="OFF"){
+      document.getElementById("Status").innerHTML =  "Load is OFF."; 
+    }
+  });
+
+  microgear.on('connected', function() {
+    microgear.setAlias(ALIAS);
+    document.getElementById("connected_NETPIE").innerHTML = "Connected to NETPIE"
+  });
+
+  microgear.on('present', function(event) {
+    console.log(event);
+  });
+
+  microgear.on('absent', function(event) {
+    console.log(event);
+  });
+
+  microgear.resettoken(function(err) {
+    microgear.connect(APPID);
+  });
+
+</script>
+
 function linen()
 {      
 	define('LINE_API',"https://notify-api.line.me/api/notify");
